8bcc122fde9dca11066fd97a7b464990ef3d1742
robert16444 <robertbylica@interia.pl>
2021-04-11 15:28:23 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ZTProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZTProject.dir/main.cpp.obj"
+  "ZTProject.exe"
+  "ZTProject.exe.manifest"
+  "ZTProject.lib"
+  "ZTProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZTProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
